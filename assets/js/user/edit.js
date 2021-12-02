b6aefaf150279d4bda939d931e80740c96f7721b
amorebietakoUdala@github.com <informatika@amorebieta.eus>
2021-12-02 08:23:49 +0100

    Fixed a style on user edit page on Edge

--- assets/js/user/edit.js
+++ assets/js/user/edit.js
@@ -1,8 +1,12 @@
 import $ from 'jquery';
 
+import '../../js/common/select2';
+
 $(function() {
     $('.js-save').on('click', function(e) {
         $(document.user).attr('action', $(e.currentTarget).data("url"));
         document.user.submit();
     });
+
+    $('.js-select2').select2();
 });
